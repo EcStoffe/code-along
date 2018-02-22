488ea6be8f779dafd7d3eaa0a14407a001217e19
Christoffer <jo.christoffer@gmail.com>
2018-02-22 10:09:17 +0100

    lagt till ny kod

---
+++ script/index.js
@@ -0,0 +1,29 @@
+let message = document.getElementById("message");
+let button = document.getElementById("submit");
+let output = document.getElementById("messageOutput");
+
+let messages = [];
+
+let currentDate = new Date();
+
+currentDate.getFullYear();
+currentDate.getMonth();
+currentDate.getDay();
+
+currentDate.getHours();
+currentDate.getMinutes();
+currentDate.getSeconds();
+
+button.addEventListener('click', function() {
+    messages.push({
+        message: message.value,
+        timeStamp: currentDate
+    });
+    output.innerHTML = "";
+    for(let message of messages) {
+        output.innerHTML += `Your messages:
+        <p>Message: ${message.message}</p>
+        <p>Time: ${message.timeStamp}</p>`
+    }
+    message.value = "";
+});
